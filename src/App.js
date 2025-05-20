c4faa73b150ab535d6c2f1ca77330f5a64a9c93a
Jainish Dabgar <jainishdabgar2901@gmail.com>
2025-05-20 13:32:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
             </audio>
             <button
               id="musicToggle"
-              className="btn btn-outline-danger mb-5"
+              className="btn btn-outline-danger mb-2 mt--3"
               onClick={toggleMusic}
             >
               {isPlaying ? "Pause Music" : "Play Music"}
